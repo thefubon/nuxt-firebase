f5c0314ae5b919d2b12d44e606e1889d8eb8751c
thefubon <43171309+thefubon@users.noreply.github.com>
2021-08-29 22:42:19 +0300

    Обновил Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "nuxt-form",
+    title: "Nuxt Firebase Form",
     htmlAttrs: {
       lang: "en"
     },
